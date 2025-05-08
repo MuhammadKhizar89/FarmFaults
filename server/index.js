385b5da8891071f2845eb960f9a178c0767a554a
Muhammad Khizar <muhammadkhizar8919@gmail.com>
2025-05-08 19:08:52 +0500

    Error Enum API created

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import connectDB from "./db.js";
 import dotenv from "dotenv";
+import errorEnumRouter from "./routes/errorEnum.route.js";
 const app = express();
 const PORT = process.env.PORT || 5000;
 dotenv.config();
@@ -21,9 +22,7 @@ app.get("/", (req, res) => {
 app.use("/api/auth", (req, res) => {
     return res.status(200).send({ message: "Hello World" });
 });
-app.use("/api/error", (req, res) => {
-    return res.status(200).send({ message: "Hello World" });
-});
+app.use("/api/error-enum", errorEnumRouter);
 app.use("/api/dashboard", (req, res) => {
     return res.status(200).send({ message: "Hello World" });
 });
